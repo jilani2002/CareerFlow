8a3bc636a242c882cfa92f5c994e3f1623a5b229
jilani2002 <syedjilani2208@gmail.com>
2026-08-22 12:19:37 +0530

    Initial CareerFlow project setup

---
+++ src/main/java/com/careerflow/CareerFlowApplication.java
@@ -0,0 +1,13 @@
+package com.careerflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerFlowApplication.class, args);
+	}
+
+}
